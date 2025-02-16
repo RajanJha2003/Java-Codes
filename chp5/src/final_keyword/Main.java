8835f5f74bf15bf2c5f8dafa31cc5a90f4db7def
RajanJha2003 <rajanj448@student.sfit.ac.in>
2025-02-16 19:05:20 +0530

    final keyword

---
+++ chp5/src/final_keyword/Main.java
@@ -0,0 +1,21 @@
+package final_keyword;
+
+final class Emp{
+	final int a=10;
+	
+	
+	 final void displayInfo() {
+		// TODO Auto-generated method stub
+		System.out.println(a);
+
+	}
+}
+
+
+
+
+
+
+public class Main {
+
+}
