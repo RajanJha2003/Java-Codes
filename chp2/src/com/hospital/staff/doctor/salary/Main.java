2d18cff4a9e6bc065381408c1accec5a1d06e7e8
RajanJha2003 <rajanj448@student.sfit.ac.in>
2025-02-16 17:41:45 +0530

    pojo

--- chp2/src/com/hospital/staff/doctor/salary/Main.java
+++
@@ -1,8 +0,0 @@
-package com.hospital.staff.doctor.salary;
-
-public class Main {
-	public static void main(String[] args) {
-		Salary salary = new Salary();
-		System.out.println(salary.calculateSalary(10000, 2000));
-	}
-}
--- chp2/src/com/hospital/staff/doctor/salary/Salary.java
+++
@@ -1,7 +0,0 @@
-package com.hospital.staff.doctor.salary;
-
-public class Salary{
-	protected double calculateSalary(double amt,double tax) {
-		return amt-tax;
-	}
-}
