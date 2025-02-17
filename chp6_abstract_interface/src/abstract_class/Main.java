2744a1a4af85ca2fdfc128c2254c962724ed529e
RajanJha2003 <rajanj448@student.sfit.ac.in>
2025-02-17 21:27:43 +0530

    abstract class

---
+++ chp6_abstract_interface/src/abstract_class/Main.java
@@ -0,0 +1,34 @@
+package abstract_class;
+
+abstract class Company {
+
+	abstract void salary();
+
+	static void companyInfo() {
+		System.out.println("XYZ company");
+	}
+
+	void displayEmpDetails() {
+		System.out.println("details");
+	}
+}
+
+class Employee extends Company {
+
+	@Override
+	void salary() {
+		System.out.println("Salary:100000");
+
+	}
+
+}
+
+public class Main {
+	public static void main(String[] args) {
+		Employee employee = new Employee();
+		employee.salary();
+		employee.displayEmpDetails();
+	Employee.companyInfo();
+
+	}
+}
