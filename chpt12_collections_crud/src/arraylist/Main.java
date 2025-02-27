cb0e44ff5f4c7f2650d65e7608b459df4562dd65
RajanJha2003 <rajanj448@student.sfit.ac.in>
2025-02-27 22:09:27 +0530

    arraylist crud

---
+++ chpt12_collections_crud/src/arraylist/Main.java
@@ -0,0 +1,34 @@
+package arraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+	public static void main(String[] args) {
+		Emp emp=new Emp(10, "Rajan", 1000000);
+		Emp emp1=new Emp(11, "Tom", 250000);
+		Emp emp2=new Emp(12, "Jerry", 300000);
+		Emp emp3=new Emp(13, "Harry", 150000);
+		
+		List<Emp> list=new ArrayList<Emp>();
+		list.add(emp);
+		list.add(emp1);
+		list.add(emp2);
+		list.add(emp3);
+		
+		OperationsImp imp=new OperationsImp();
+		imp.insertData(list);
+		imp.showData();
+		System.out.println("---------------------------------");
+	    imp.updateData(13, "Kary");
+	    imp.showData();
+	    System.out.println("--------------------------------");
+	    imp.deleteData(12);
+	    imp.showData();
+	    System.out.println("----------------------------------------");
+	    imp.searchData(10);
+	    
+	}
+	
+
+}
